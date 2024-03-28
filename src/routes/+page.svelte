d3c97e66faaf14964ed4b7ee8078810757607e2d
brycewoodland <bwoodlan@byui.edu>
2024-03-28 14:37:59 -0600

    CollectionList

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,9 @@
 <script>
     import Header from "../lib/components/Header.svelte";
-
+    import CollectionsView from "../lib/components/CollectionsView.svelte";
 
 </script>
 
 <Header />
 <h1>Landing Page</h1>
+<CollectionsView />
